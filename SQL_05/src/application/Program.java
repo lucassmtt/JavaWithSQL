e50c517867fd62ed5eaa46a83ab56c0f89126015
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-06-19 13:11:26 -0300

    Update Program.java

--- SQL_05/src/application/Program.java
+++ SQL_05/src/application/Program.java
@@ -23,20 +23,17 @@ public class Program
             statement = connection.createStatement();
 
             int rows_affect_1 = statement.executeUpdate(
-                    "UPDATE base_de_dados.seller " +
-                         "SET BaseSalary = 2000000 " +
-                         "WHERE DepartmentId = 1;"
+                    "UPDATE base_de_dados.seller SET BaseSalary = 20000 WHERE (DepartmentId = 1);"
             );
 
             int rows_affect_2 = statement.executeUpdate(
-                    "UPDATE base_de_dados.seller " +
-                            "SET BaseSalary = BaseSalary * 3" +
-                            "WHERE DepartmentId = 2"
+                    "UPDATE base_de_dados.seller SET BaseSalary = BaseSalary * 3 WHERE (DepartmentId = 2);"
             );
 
             System.out.println("You confirm the operation? ");
             String response = scanner.next().toUpperCase();
             if (response.equals("S")){
+                System.out.println("Rows affect " + (rows_affect_1 + rows_affect_2));
                 connection.commit();
             }
             else {
@@ -45,10 +42,13 @@ public class Program
         }
         catch (Exception e){
             try {
-                connection.rollback();
+                if (connection != null){
+                    connection.rollback();
+                    throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+                }
             }
             catch (SQLException e1){
-                throw new DbException(e1.getMessage());
+                throw new DbException("Error in rollback! Caused by: " + e1.getMessage());
             }
         }
         finally {
