76bd3dc74d17e78ecea2a0a1bb49848af848c2a8
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-06-19 15:39:15 -0300

    Create interfaces to the our project

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program
 {
     public static void main(String[] args)
     {
         Department department = new Department(1, "Gym");
+        Seller seller = new Seller(21, "Jon", "jon@gmail.con", new Date(), 2000.0, department);
+
         System.out.println(department);
+        System.out.println(seller);
     }
 }
